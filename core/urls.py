cb1742de9bd2d781cbb73eccf09b3526b027a3c3
Javokhirbek <khaydaraliev99@gmail.com>
2021-08-06 14:00:49 +0300

    secret key fix

--- core/urls.py
+++ core/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     path('auth-user/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('blogapi.urls', namespace='posts')),
     path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
