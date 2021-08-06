a492769d591d0745b3f80d38f5f59dc16706c75d
Javokhirbek <khaydaraliev99@gmail.com>
2021-08-06 13:51:54 +0300

    secret key fix

--- core/settings.py
+++ core/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.1.107']
+
+ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.1.107',]
 
 
 # Application definition
